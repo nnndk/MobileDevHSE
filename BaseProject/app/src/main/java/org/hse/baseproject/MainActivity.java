222df1850380056ec8eb330aa87b3f786bd5225c
Danil <danil17070216@gmail.com>
2022-11-20 11:06:45 +0500

    fix base project

--- BaseProject/app/src/main/java/org/hse/baseproject/MainActivity.java
+++ BaseProject/app/src/main/java/org/hse/baseproject/MainActivity.java
@@ -3,11 +3,32 @@ package org.hse.baseproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.main_activity);
+
+        View button = findViewById(R.id.button);
+        View button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            { showToast("Расписание для преподавателей"); };
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            { showToast("Расписание для студентов"); };
+        });
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(getApplicationContext(), text,
+                Toast.LENGTH_SHORT).show();
     }
 }
